df48e6009f973f57e006c9756eaa7bd594c2c779
Juan <juanalvear2009@gmail.com>
2023-11-10 22:02:44 -0500

    Proyecto integrador parte 2

--- proyecto _integrador/main.py
+++ proyecto _integrador/main.py
@@ -1,2 +1,18 @@
+import readchar
+
 nombre_participante = input("Ingrese su nombre para iniciar: \n")
 print(f"Bienvenido al Juego {nombre_participante}")
+
+# Bucle infinito
+while True:
+
+    # Lee un caracter del teclado
+    caracter = readchar.readkey()
+
+    # Imprime el caracter leído
+    print(f"{nombre_participante} presiona la tecla {caracter}")
+
+    # Termina el bucle si se presiona la tecla ↑
+    if caracter == readchar.key.UP :
+        print("¡Tecla hacia arriba presionada! Terminando el juego.")
+        break
