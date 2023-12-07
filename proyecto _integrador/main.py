ac2ef1e94632be7c5b2cf43c4eef8c80228ee69a
Juan <juanalvear2009@gmail.com>
2023-12-07 11:36:53 -0500

    funcion para limpiar terminal y aunmentar numero

--- proyecto _integrador/main.py
+++ proyecto _integrador/main.py
@@ -1,7 +1,20 @@
 import readchar
+import os
+
+def limpiar_terminal():
+    # Borrar la terminal según el sistema operativo
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def imprimir_numero_y_limpiar(nombre_participante, numero):
+    limpiar_terminal()
+    print(f"{nombre_participante} presiona la tecla 'n'")
+    print(f"Número actual: {numero}")
 
 nombre_participante = input("Ingrese su nombre para iniciar: \n")
 print(f"Bienvenido al Juego {nombre_participante}")
+
+
+numero = 0
 
 # Bucle infinito
 while True:
@@ -15,4 +28,14 @@ while True:
     # Termina el bucle si se presiona la tecla ↑
     if caracter == readchar.key.UP :
         print("¡Tecla hacia arriba presionada! Terminando el juego.")
+        break
+
+    # Incrementar el número si se presiona la tecla 'n'
+    if caracter == 'n':
+        numero += 1
+        imprimir_numero_y_limpiar(nombre_participante, numero)
+
+    # Rompe el bucle si se alcanza el número 50
+    if numero == 50:
+        print("¡Has alcanzado el número 50! Terminando el juego.")
         break
